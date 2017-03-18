3ed1e6b219c1cceb88c546dc308c44ebd2a88bc5
chiaraercolani <chiara.ercolani8@gmail.com>
2017-03-18 04:02:28 +0100

    try

--- app/src/main/java/com/example/aurelien/travelnow/MainActivity.java
+++ app/src/main/java/com/example/aurelien/travelnow/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.aurelien.travelnow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int CALENDAR_ACTIVITY=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view){
+        Intent intent;
+        switch(view.getId()){
+            case R.id.btn_init:
+                intent = new Intent(this,CalendarActivity.class);
+                startActivityForResult(intent,CALENDAR_ACTIVITY);
+                break;
+
+        }
+
+
     }
 }
